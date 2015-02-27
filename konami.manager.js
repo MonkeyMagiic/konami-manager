edd36ad95a52eb602cc6247a80a7b7c17d9aefab
Tyrone Neill <tyrone_neill@yahoo.co.uk>
2015-02-27 20:24:34 +0000

    Adding the keyboard class

--- konami.manager.js
+++ konami.manager.js
@@ -14,92 +14,83 @@
 (function (global) {
     'use strict';
 
-    return (function () {
+    /**
+     * Konami Manager
+     *
+     * @param stage
+     * @constructor
+     */
+    function KonamiManager() {
 
         /**
-         * Konami Manager
-         *
-         * @param stage
-         * @constructor
+         * Collection of konami items
+         * @type {Array}
+         * @private
          */
-        function KonamiManager(stage) {
-
-            /**
-             * Collection of konami items
-             * @type {Array}
-             * @private
-             */
-            var _konamis = [];
-
-            /**
-             * Add a combination to result.....
-             *
-             * @param result callback when the <code>combination</code> has been executed successfully.
-             * @param combination sequence of characters (usually keyboard codes).
-             */
-            KonamiManager.prototype.add = function (result, combination) {
-                _konamis.push(new Konami(result, combination));
-            };
-
-            /**
-             * @private
-             */
-            stage.addEventListener('keydown', function (event) {
-
-                var i = 0;
-                do {
-                    _konamis[i].testSequence(event.keyCode);
-                    i++;
-                } while (i < _konamis.length);
-            });
-        }
-
-        ////// HACKING
-        window.konamiManager = new KonamiManager(window);
-        /*
-         return {konamiManager: new KonamiManager(window)};
-
-         */
+        var _konamis = [];
 
         /**
-         * Konami
+         * Add a konami by specifying both the combination/sequence and the result handler.
          *
          * @param result callback when the <code>combination</code> has been executed successfully.
          * @param combination sequence of characters (usually keyboard codes).
-         * @constructor
          */
-        function Konami(result, combination) {
+        KonamiManager.prototype.add = function (result, combination) {
+            _konamis.push(new Konami(result, combination));
+        };
 
-            /**
-             * Current position in the <code>combination</code>.
-             * @type {number}
-             */
-            var position = 0;
+        /**
+         * @private
+         */
+        window.addEventListener('keydown', function (event) {
 
-            /**
-             * @private
-             */
-            function complete() {
-                result && result();
-                position = 0;
-            }
+            var i = 0;
+            do {
+                _konamis[i].testSequence(event.keyCode);
+                i++;
+            } while (i < _konamis.length);
+        });
+    } // End of KonamiManager.
 
-            /**
-             * Test a given code against the current <code>position</code>
-             *  within the <code>combination</code>.
-             * @param code
-             */
-            Konami.prototype.testSequence = function (code) {
-                if (combination[position++] === code) {
-                    if (position === combination.length) {
-                        complete();
-                    }
-                } else {
+    /**
+     * Konami
+     *
+     * @param result callback when the <code>combination</code> has been executed successfully.
+     * @param combination sequence of characters (usually keyboard codes).
+     * @constructor
+     */
+    function Konami(result, combination) {
+
+        /**
+         * Current position in the <code>combination</code>.
+         * @type {number}
+         */
+        var position = 0;
+
+        /**
+         * Test a given code against the current <code>position</code>
+         *  within the <code>combination</code>.
+         * @param code
+         */
+        Konami.prototype.testSequence = function (code) {
+            if (combination[position++] === code) {
+                if (position === combination.length) {
+                    result && result();
                     position = 0;
                 }
-            };
-        } // End of Konami.
+            } else {
+                position = 0;
+            }
+        };
+    } // End of Konami.
 
-    })();
+    /**
+     * Attach library to window.
+     */
+    if (typeof define === "function" && define.amd) {
+        define(new KonamiManager());
+    } else {
+        window.konamiManager = new KonamiManager();
+    }
 
 })(this);
